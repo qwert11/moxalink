d0336a8dac19308f06cbf3dc573144758a57ae46
qwert11 <qwert11_@mail.ru>
2012-08-11 13:18:38 +0200

    добавляю все текстовые файлы

---
+++ MainFrm.dfm
@@ -0,0 +1,201 @@
+object frmMain: TfrmMain
+  Left = 476
+  Top = 192
+  Width = 918
+  Height = 517
+  Caption = #1052#1086#1082#1089#1072' '#1082#1083#1080#1077#1085#1090
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  Menu = mm1
+  OldCreateOrder = False
+  OnActivate = FormActivate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object mmoLog: TMemo
+    Left = 0
+    Top = 105
+    Width = 910
+    Height = 347
+    Align = alClient
+    Lines.Strings = (
+      'mmoLog')
+    TabOrder = 0
+  end
+  object pnl1: TPanel
+    Left = 0
+    Top = 0
+    Width = 910
+    Height = 105
+    Align = alTop
+    TabOrder = 1
+    DesignSize = (
+      910
+      105)
+    object edt1: TEdit
+      Left = 16
+      Top = 16
+      Width = 121
+      Height = 21
+      Hint = 'Press ENTER'
+      MaxLength = 1
+      TabOrder = 0
+      OnChange = edt1Change
+      OnClick = edt1Click
+      OnKeyPress = edt1KeyPress
+    end
+    object btn4: TButton
+      Left = 104
+      Top = 48
+      Width = 75
+      Height = 25
+      Caption = 'backspase'
+      TabOrder = 1
+      OnClick = btn4Click
+    end
+    object btn3: TButton
+      Left = 16
+      Top = 48
+      Width = 75
+      Height = 25
+      Caption = 'Send'
+      TabOrder = 2
+      OnClick = btn3Click
+    end
+    object btnDisconnectMoxa: TButton
+      Left = 742
+      Top = 64
+      Width = 75
+      Height = 25
+      Anchors = [akTop, akRight]
+      Caption = 'Disconnect'
+      TabOrder = 3
+      OnClick = btnDisconnectMoxaClick
+    end
+    object btnConnectMoxa: TButton
+      Left = 742
+      Top = 24
+      Width = 75
+      Height = 25
+      Anchors = [akTop, akRight]
+      Caption = 'Connect'
+      TabOrder = 4
+      OnClick = btnConnectMoxaClick
+    end
+    object btn5: TButton
+      Left = 16
+      Top = 80
+      Width = 75
+      Height = 25
+      Caption = 'ReadText'
+      TabOrder = 5
+      OnClick = btn5Click
+    end
+    object btn6: TButton
+      Left = 104
+      Top = 80
+      Width = 75
+      Height = 25
+      Caption = 'ReadLine'
+      TabOrder = 6
+      OnClick = btn6Click
+    end
+    object chkConvert: TCheckBox
+      Left = 304
+      Top = 48
+      Width = 97
+      Height = 17
+      Caption = 'chkConvert'
+      TabOrder = 7
+      OnClick = chkConvertClick
+    end
+    object chkDel0: TCheckBox
+      Left = 304
+      Top = 80
+      Width = 97
+      Height = 17
+      Caption = 'chkDel0'
+      Checked = True
+      State = cbChecked
+      TabOrder = 8
+      OnClick = chkDel0Click
+    end
+    object chkConvertVithOrig: TCheckBox
+      Left = 304
+      Top = 24
+      Width = 129
+      Height = 17
+      Caption = 'chkConvertVithOrig'
+      TabOrder = 9
+      OnClick = chkConvertVithOrigClick
+    end
+    object chkStik: TCheckBox
+      Left = 432
+      Top = 24
+      Width = 97
+      Height = 17
+      Caption = 'chkSti'#1089'k'
+      TabOrder = 10
+    end
+    object btnAllData: TButton
+      Left = 192
+      Top = 48
+      Width = 75
+      Height = 25
+      Caption = 'AllData'
+      TabOrder = 11
+      OnClick = btnAllDataClick
+    end
+    object btnReadLn: TButton
+      Left = 192
+      Top = 80
+      Width = 75
+      Height = 25
+      Caption = 'ReadLn'
+      TabOrder = 12
+      OnClick = btnReadLnClick
+    end
+  end
+  object stat1: TStatusBar
+    Left = 0
+    Top = 452
+    Width = 910
+    Height = 19
+    Panels = <
+      item
+        Width = 150
+      end
+      item
+        Width = 150
+      end
+      item
+        Width = 50
+      end>
+  end
+  object idtlnt1: TIdTelnet
+    MaxLineAction = maException
+    ReadTimeout = 0
+    Host = '192.168.1.251'
+    Port = 23
+    Terminal = 'dumb'
+    Left = 72
+    Top = 16
+  end
+  object dlgOpen1: TOpenDialog
+    Left = 464
+    Top = 56
+  end
+  object mm1: TMainMenu
+    Left = 336
+    Top = 288
+    object File1: TMenuItem
+      Caption = 'File'
+    end
+    object Option1: TMenuItem
+      Caption = 'Option'
+    end
+  end
+end
---
+++ MainFrm.pas
@@ -0,0 +1,343 @@
+unit MainFrm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, IdBaseComponent, IdComponent, IdTCPConnection, IdTCPClient,
+  IdTelnet, StdCtrls, ExtCtrls, IdGlobal, ComCtrls, Menus;
+
+type
+  TfrmMain = class(TForm)
+    idtlnt1: TIdTelnet;
+    btnConnectMoxa: TButton;
+    btnDisconnectMoxa: TButton;
+    btn3: TButton;
+    edt1: TEdit;
+    mmoLog: TMemo;
+    btn4: TButton;
+    pnl1: TPanel;
+    btn5: TButton;
+    dlgOpen1: TOpenDialog;
+    btn6: TButton;
+    chkConvert: TCheckBox;
+    chkDel0: TCheckBox;
+    stat1: TStatusBar;
+    chkConvertVithOrig: TCheckBox;
+    chkStik: TCheckBox;
+    btnAllData: TButton;
+    btnReadLn: TButton;
+    mm1: TMainMenu;
+    File1: TMenuItem;
+    Option1: TMenuItem;
+    procedure btnConnectMoxaClick(Sender: TObject);
+    procedure btnDisconnectMoxaClick(Sender: TObject);
+    procedure btn3Click(Sender: TObject);
+    procedure btn4Click(Sender: TObject);
+    procedure btn5Click(Sender: TObject);
+    procedure btn6Click(Sender: TObject);
+    procedure edt1KeyPress(Sender: TObject; var Key: Char);
+    procedure edt1Change(Sender: TObject);
+    procedure edt1Click(Sender: TObject);
+    procedure chkConvertClick(Sender: TObject);
+    procedure chkDel0Click(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure chkConvertVithOrigClick(Sender: TObject);
+    procedure btnAllDataClick(Sender: TObject);
+    procedure btnReadLnClick(Sender: TObject);
+  private
+    { Private declarations }
+    procedure SendChFromStr(s: string);
+    procedure GetText;
+    procedure GetTextWaitFor;
+    procedure GetTextAllData;
+    procedure GetTextReadLn;
+    procedure GetLine;
+    procedure ConnectToMoxa;
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmMain.btnConnectMoxaClick(Sender: TObject);
+begin
+  ConnectToMoxa
+end;
+
+procedure TfrmMain.btnDisconnectMoxaClick(Sender: TObject);
+begin
+  if idtlnt1.Connected then
+    idtlnt1.Disconnect;
+  mmoLog.Clear;
+end;
+
+procedure TfrmMain.btn3Click(Sender: TObject);
+var
+  ch: Char;
+begin
+  ch := #13;
+  edt1KeyPress(nil, ch)
+end;
+
+procedure TfrmMain.SendChFromStr(s: string);
+var
+  I: Integer;
+begin
+  for I := 1 to Length(s) do
+    idtlnt1.SendCh(s[I]);
+  idtlnt1.SendCh(#13);
+end;
+
+{1
+3
+2111
+5 раз энтер }
+
+procedure TfrmMain.btn4Click(Sender: TObject);
+begin
+  idtlnt1.SendCh(#8);
+end;
+
+function Del_0(const s: string): string;
+var
+  I: Integer;
+begin
+  Result := '';
+  for I := 1 to Length(s) do
+    if not (s[I] in [#0]) then
+      Result := Result + s[I];
+end;
+
+function ConvertStr(s: string): string;
+var
+  I: Integer;
+begin
+  Result := '';
+  for I := 1 to Length(s) do
+    Result := Result + '<' + IntToStr(Ord(s[I])) + '>'
+end;
+
+function ConvertStrStick(s: string): string;
+var
+  I: Integer;
+begin
+  Result := '';
+  for I := 1 to Length(s) do
+    Result := Result + s[I] + '<' + IntToStr(Ord(s[I])) + '>'
+end;
+
+
+
+procedure TfrmMain.GetText;                                   {readmemorystring   stringstring}
+begin
+  GetTextWaitFor
+end;
+
+{procedure TForm1.GetText;                                   //readmemorystring   stringstring
+var
+  s: string;                                                                  //readwait for
+  //ch: Char;
+  I: Integer;
+begin
+  //mmoLog.Lines.Add('InputLn: ' + idtlnt1.InputLn());            //#10#13#10#13
+  //mmoLog.Lines.Add('AllData: ' + idtlnt1.AllData);
+  //mmoLog.Lines.Add('Greeting.Text: ');
+  //mmoLog.Lines.AddStrings(idtlnt1.Greeting.Text);
+  //mmoLog.Lines.Add('CurrentReadBuffer: ' + idtlnt1.CurrentReadBuffer);
+  //mmoLog.Lines.Add('ReadString: ' + idtlnt1.ReadString(15));
+
+//  repeat
+    //if idtlnt1.ReadChar = #13 then
+    //ch := idtlnt1.ReadChar;
+
+    //mmoLog.Lines.Add('ReadChar: ' + ch);
+//    s := idtlnt1.ReadLn();
+//    s := idtlnt1.ReadLn(#10);
+    s := idtlnt1.WaitFor(#13#0);
+    if chkConvert.Checked then
+      s := ConvertStr(s);
+    if chkDel0.Checked then
+      s := Del_0(s);
+
+    //s := ConvertStr(s);
+    //s := idtlnt1.r
+//    if Pos(#13, s) > 0 then
+//      Delete(s, Pos(#13, s), 1);
+
+
+    //mmoLog.Lines.Text := s;
+    mmoLog.Lines.Add('Length(s): ' + IntToStr(Length(s)) + ', ReadLn: ' + s);
+//
+//    for I := 1 to Length(s) do
+//      mmoLog.Lines.Add(s[I]);
+//  until (s[1] = #13) or (Length(s) < 3);// and (idtlnt1.ReadChar = #13);
+end;}
+
+procedure TfrmMain.btn5Click(Sender: TObject);
+begin
+  GetText
+end;
+
+procedure TfrmMain.GetLine;
+var
+  s: string;
+begin
+  s := idtlnt1.ReadLn(#10);
+  if (chkConvert.Checked) and (chkStik.Checked) then begin
+    s := ConvertStrStick(Del_0(s))
+  end else
+  if chkConvert.Checked then
+    s := ConvertStr(s);
+  if chkDel0.Checked then
+    s := Del_0(s);
+  mmoLog.Lines.Add('Length(s): ' + IntToStr(Length(s)) + ', ReadLn: ' + s);
+end;
+
+procedure TfrmMain.btn6Click(Sender: TObject);
+begin
+  GetLine
+end;
+
+procedure TfrmMain.edt1KeyPress(Sender: TObject; var Key: Char);
+begin
+  if (Key = #13) and (idtlnt1.Connected) then
+    begin
+      SendChFromStr(edt1.Text);
+      edt1.Clear;
+      GetText;
+    end;
+end;
+
+procedure TfrmMain.edt1Change(Sender: TObject);
+var
+  s: string;
+begin
+  if Length(edt1.Text) > 1 then begin
+    s := edt1.Text;
+    edt1.Text := s[Length(s)];
+  end;
+  edt1.SelectAll  
+end;
+
+procedure TfrmMain.edt1Click(Sender: TObject);
+begin
+  edt1.SelectAll
+end;
+
+procedure TfrmMain.chkConvertClick(Sender: TObject);
+begin
+  chkDel0.Checked := not chkConvert.Checked;
+  chkConvertVithOrig.Checked := chkConvert.Checked;
+end;
+
+procedure TfrmMain.chkDel0Click(Sender: TObject);
+begin
+  chkConvert.Checked := not chkDel0.Checked
+end;
+
+procedure TfrmMain.ConnectToMoxa;
+begin
+  if idtlnt1.Connected then
+    Exit;
+  idtlnt1.Connect;
+  mmoLog.Clear;
+  GetText
+end;
+
+procedure TfrmMain.FormActivate(Sender: TObject);
+begin
+  WindowState := wsMaximized;
+  ConnectToMoxa
+end;
+
+procedure TfrmMain.chkConvertVithOrigClick(Sender: TObject);
+begin
+  chkStik.Checked := chkConvertVithOrig.Checked
+end;
+
+procedure TfrmMain.GetTextAllData;
+var
+  s,
+  sConvert: string;                                                                  {readwait for}
+begin
+  s := idtlnt1.AllData;
+
+  if (not chkConvert.Checked) and (chkDel0.Checked) then
+    sConvert := Del_0(s);
+  if (chkConvert.Checked) and (chkStik.Checked) then begin
+    sConvert := Del_0(s);
+    sConvert := ConvertStrStick(sConvert);
+  end else
+  if (chkConvert.Checked) then
+    sConvert := ConvertStr(s);
+  mmoLog.Lines.Add('Length(s): ' + IntToStr(Length(s)) + ', ReadLn: ' + sConvert);
+  stat1.Panels[2].Text := 'Length(s): ' + IntToStr(Length(s));
+  if chkConvertVithOrig.Checked then begin
+    s := Del_0(s);
+    mmoLog.Lines.Add('Original Length(s): ' + IntToStr(Length(s)) + ', ReadLn: ' + s);
+  end;
+end;
+
+procedure TfrmMain.btnAllDataClick(Sender: TObject);
+begin
+  GetTextAllData
+end;
+
+procedure TfrmMain.btnReadLnClick(Sender: TObject);
+begin
+  GetTextReadLn
+end;
+
+procedure TfrmMain.GetTextReadLn;
+var
+  s,
+  sConvert: string;                                                                  {readwait for}
+begin
+  s := idtlnt1.ReadLn();
+
+  if (not chkConvert.Checked) and (chkDel0.Checked) then
+    sConvert := Del_0(s);
+  if (chkConvert.Checked) and (chkStik.Checked) then begin
+    sConvert := Del_0(s);
+    sConvert := ConvertStrStick(sConvert);
+  end else
+  if (chkConvert.Checked) then
+    sConvert := ConvertStr(s);
+  mmoLog.Lines.Add('Length(s): ' + IntToStr(Length(s)) + ', ReadLn: ' + sConvert);
+  stat1.Panels[2].Text := 'Length(s): ' + IntToStr(Length(s));
+  if chkConvertVithOrig.Checked then begin
+    s := Del_0(s);
+    mmoLog.Lines.Add('Original Length(s): ' + IntToStr(Length(s)) + ', ReadLn: ' + s);
+  end;
+end;
+
+procedure TfrmMain.GetTextWaitFor;
+var
+  s,
+  sConvert: string;                                                                  {readwait for}
+begin
+  s := idtlnt1.WaitFor(#13#0);
+  if (not chkConvert.Checked) and (chkDel0.Checked) then
+    sConvert := Del_0(s);
+  if (chkConvert.Checked) and (chkStik.Checked) then begin
+    sConvert := Del_0(s);
+    sConvert := ConvertStrStick(sConvert);
+  end else
+  if (chkConvert.Checked) then
+    sConvert := ConvertStr(s);
+  mmoLog.Lines.Add('Length(s): ' + IntToStr(Length(s)) + ', ReadLn: ' + sConvert);
+  stat1.Panels[2].Text := 'Length(s): ' + IntToStr(Length(s));
+  if chkConvertVithOrig.Checked then begin
+    s := Del_0(s);
+    mmoLog.Lines.Add('Original Length(s): ' + IntToStr(Length(s)) + ', ReadLn: ' + s);
+  end;
+
+end;
+
+end.
